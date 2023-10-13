525e1b53dfd42f2ab4928046ff30098529aaf765
thangtedao <thangtedao@gmail.com>
2023-10-13 21:59:58 +0700

    web

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Register = () => {
+  const Wrapper = styled.main``;
+
   return (
-    <h2>
-      <Link to="/login">Login Page</Link>
-    </h2>
+    <Wrapper>
+      <form className="form">
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-input"
+            defaultValue="thang"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
