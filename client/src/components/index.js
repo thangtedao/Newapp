3bdf9e9992b52d14ec0207b6ce3b44001f17a936
thangtedao <thangtedao@gmail.com>
2023-10-19 21:32:38 +0700

    web

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from "../components/Navbar";
 export { default as BigSideBar } from "../components/BigSideBar";
 export { default as SmallSideBar } from "../components/SmallSideBar";
 export { default as FormRow } from "../components/FormRow";
+export { default as FormRowSelect } from "../components/FormRowSelect";
--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,6 +1,6 @@
-import { FormRow } from "../components";
+import { FormRow, FormRowSelect } from "../components";
 import { useOutletContext } from "react-router-dom";
-//import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
 import { Form, useNavigation, redirect } from "react-router-dom";
 import { toast } from "react-toastify";
 import customFetch from "../utils/customFetch";
@@ -27,6 +27,18 @@ const AddJob = () => {
             lableText="job location"
             name="jobLocation"
             defaultValue={user.location}
+          />
+          <FormRowSelect
+            labelText="job status"
+            name="jobStatus"
+            defaultValue={JOB_STATUS.PENDING}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            labelText="job type"
+            name="jobType"
+            defaultValue={JOB_TYPE.FULL_TIME}
+            list={Object.values(JOB_TYPE)}
           />
           <button
             type="submit"
